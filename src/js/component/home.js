0f0604b1381b4dd1fe79f8c5d424aa6e4ae16693
Kamari1974 <karen197499@gmail.com>
2021-04-30 20:39:26 +0000

    Todo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [deber, setDeber] = useState("");
+	const [list, setList] = useState([]);
+
+	function eliminar(index) {
+		const deletelist = list.filter((item, key) => index !== key);
+		setList(deletelist);
+	}
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="display-5 text">Lista de Tareas</h1>
+			<input
+				value={deber}
+				onChange={e => {
+					setDeber(e.target.value);
+				}}
+				onKeyPress={e => {
+					if (e.key == "Enter") {
+						setList(list.concat(deber));
+						setDeber("");
+					}
+				}}
+			/>
+			{list.map((item, index) => {
+				return (
+					<li key={index}>
+						{item}
+						<button
+							onClick={() => eliminar(index)}
+							type="button"
+							className="btn btn-link">
+							X
+						</button>
+					</li>
+				);
+			})}
 		</div>
 	);
 }
